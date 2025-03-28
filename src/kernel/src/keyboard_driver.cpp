9f6c7f695b6af2daf12dc2f3b7c8144f179748f7
cactoes <maartenhvanrooij@gmail.com>
2025-03-28 19:43:48 +0100

    added basic keyboard handler

---
+++ src/kernel/src/keyboard_driver.cpp
@@ -0,0 +1,39 @@
+#include "keyboard_driver.hpp"
+#include "cpu.hpp"
+
+extern void critical_fatal(uint64_t code, const char* message);
+
+key_state_t* g_key_states = nullptr;
+
+void keyboard_init(key_state_t keystates[KEY_STATE_ARRAY_SIZE]) {
+    g_key_states = keystates;
+}
+
+key_state_t* keyboard_get_key_state(uint64_t scan_code) {
+    if (scan_code >= KEY_STATE_ARRAY_SIZE)
+        return nullptr;
+
+    return &g_key_states[scan_code];
+}
+
+cpu_state_t* keyboard_handle_interrupt(uint64_t code, cpu_state_t* rsp) {
+    key_state_t key_state {};
+    key_state.full_code = cpu_inb(KEYBOARD_PORT_SCANCODE);
+
+    if (key_state.full_code == FULL_CODE_ESCAPED) {
+        key_state.is_escaped = true;
+        key_state.full_code = cpu_inb(KEYBOARD_PORT_SCANCODE);
+    }
+
+    key_state.scan_code = (key_state.full_code & SCAN_CODE_MASK_CHAR);
+    key_state.is_released = (key_state.full_code & SCAN_CODE_MASK_PRESSED) != 0;
+
+    if (key_state.scan_code < KEY_STATE_ARRAY_SIZE) {
+        g_key_states[key_state.scan_code] = key_state;
+    } else {
+        critical_fatal(key_state.scan_code, "FATAL (keyboard): key code out of range");
+    }
+
+    cpu_outb(PIC1, 0x20);
+    return rsp;
+}
--- src/kernel/src/main.cpp
+++ src/kernel/src/main.cpp
@@ -3,6 +3,7 @@
 #include "vga_driver.hpp"
 #include "interrupt.hpp"
 #include "cpu.hpp"
+#include "keyboard_driver.hpp"
 
 void critical_fatal(uint64_t code, const char* message) {
     vga_tm_color_map_t color {
@@ -11,19 +12,19 @@ void critical_fatal(uint64_t code, const char* message) {
     };
     vga_tm_set_color(&color);
     vga_tm_clear_screen();
-    vga_tm_print("(%uh) %s \n", code, message);
+    vga_tm_print("(0x%uh) %s \n", code, message);
     
     while (true)
         cpu_halt();
 }
 
 cpu_state_t* handle_critical_interrupt(uint64_t code, cpu_state_t* rsp) {
-    critical_fatal(code, "FATAL");
+    critical_fatal(code, "FATAL (critical)");
     return rsp;
 }
 
-cpu_state_t* handle_keyboard_interrupt(uint64_t code, cpu_state_t* rsp) {
-    critical_fatal(code, "FATAL");
+cpu_state_t* handle_other_interrupt(uint64_t code, cpu_state_t* rsp) {
+    critical_fatal(code, "FATAL (other)");
     return rsp;
 }
 
@@ -34,15 +35,21 @@ extern "C" void kernel_entry(multiboot_t* multiboot_struct, void* kpml4) {
     if (multiboot_struct->magic != 0x2BADB002)
         critical_fatal(multiboot_struct->magic, "multiboot magic was invalid");
 
+    int_set_callback(interrupt_type::OTHER, handle_other_interrupt);
     int_set_callback(interrupt_type::CRITICAL, handle_critical_interrupt);
-    int_set_callback(interrupt_type::KEYBOARD, handle_keyboard_interrupt);
+    int_set_callback(interrupt_type::KEYBOARD, keyboard_handle_interrupt);
     int_init();
 
-    vmem_init(multiboot_struct, kpml4);
+    if (!vmem_init(multiboot_struct, kpml4))
+        critical_fatal(0x0, "vmem_init failed");
 
     heap_t heap {};
-    heap_init(&heap, kpml4, (void*)0x40000000, 0x100000 * 32);
+    if (!heap_init(&heap, kpml4, (void*)0x40000000, 0x100000 * 32))
+        critical_fatal(0x0, "heap_init failed");
     set_global_heap(&heap);
+
+    key_state_t key_states[0xff];
+    keyboard_init(key_states);
 
     while (true) {}
 }
