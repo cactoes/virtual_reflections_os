02b8465a12960e58d86ed5d5fa0a98a0733d5524
cactoes <maartenhvanrooij@gmail.com>
2025-05-18 23:04:28 +0200

    mutex lock guard class

--- src/kernel/include/mutex.hpp
+++ src/kernel/include/mutex.hpp
@@ -18,4 +18,27 @@ void mutex_init(mutex_t* mutex);
 void mutex_lock(mutex_t* mutex);
 void mutex_unlock(mutex_t* mutex);
 
+/// @brief helper class for managing mutex lock / unlocks automatically
+class mutex_lock_guard {
+public:
+    mutex_lock_guard(mutex_t* mutex) : _mutex(mutex) {
+        if (_mutex)
+            mutex_lock(_mutex);
+    }
+
+    ~mutex_lock_guard() {
+        if (_mutex)
+            mutex_unlock(_mutex);
+    }
+
+    // no wierd shit
+    mutex_lock_guard(const mutex_lock_guard& other) = delete;
+    mutex_lock_guard& operator=(const mutex_lock_guard& other) = delete;
+    mutex_lock_guard(mutex_lock_guard&& other) = delete;
+    mutex_lock_guard& operator=(mutex_lock_guard&& other) = delete;
+
+private:
+    mutex_t* _mutex;
+};
+
 #endif // __MUTEX_HPP__
