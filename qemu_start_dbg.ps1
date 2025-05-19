7b382e4668c267354d36caf9ee8c3b2b0f02ad28
cactoes <maartenhvanrooij@gmail.com>
2025-05-19 18:15:37 +0200

    created debug script

---
+++ qemu_start_dbg.ps1
@@ -0,0 +1,11 @@
+Write-Host "starting qemu"
+qemu-system-x86_64.exe `
+    -machine pc `
+    -cdrom build/VirtualReflectionsOS.iso `
+    -m 4G `
+    -serial stdio `
+    -drive format=raw,file=test_disk.vhd,id=disk,if=none `
+    -device ahci,id=ahci `
+    -device ide-hd,drive=disk,bus=ahci.0 `
+    -S -gdb tcp::1234 `
+    -no-reboot
