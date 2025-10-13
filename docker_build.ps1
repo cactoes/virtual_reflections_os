10e498b8952b838b78f41a2e22fb73e509308aa4
cactoes <maartenhvanrooij@gmail.com>
2025-10-14 01:14:10 +0200

    fixed tasks

--- docker_build.ps1
+++ docker_build.ps1
@@ -2,3 +2,7 @@ $CommitHash = git rev-parse --short HEAD
 
 Write-Host "running docker container" -ForegroundColor Green
 docker run --name VirtualReflectionsOS --rm -v "${PWD}:/root/env" -e GIT_COMMIT_HASH=$CommitHash virtual_reflections_os_buildenv
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
