4caef48e6108479b1144a045821bc5ff91011a75
cactoes <maartenhvanrooij@gmail.com>
2025-11-03 14:16:27 +0100

    moved terminal

---
+++ src/kernel/include/terminal.hpp
@@ -0,0 +1,13 @@
+//==========================================
+/// @file       terminal.hpp
+/// @brief      kernel terminal impl
+//==========================================
+
+#pragma once
+
+#ifndef __TERMINAL_HPP__
+#define __TERMINAL_HPP__
+
+int terminal_thread_main();
+
+#endif // __TERMINAL_HPP__
