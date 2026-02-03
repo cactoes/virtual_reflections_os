8eda72fe658438424cc1a1b16a55c459e5a95c95
cactoes <maartenhvanrooij@gmail.com>
2026-02-03 11:34:43 +0100

    void pointer class

--- src/shared_headers/std/pointer.hpp
+++ src/shared_headers/std/pointer.hpp
@@ -75,6 +75,61 @@ private:
     T* ptr;
 };
 
+template <>
+class unique_ptr<void> {
+public:
+    unique_ptr() : ptr(nullptr) {}
+    explicit unique_ptr(void* p_ptr) : ptr(p_ptr) {}
+
+    // no copy
+    unique_ptr(const unique_ptr& other) = delete;
+    unique_ptr& operator=(const unique_ptr& other) = delete;
+    
+    template <typename U>
+    unique_ptr(unique_ptr<U>&& other) {
+        ptr = other.ptr;
+        other.ptr = nullptr;
+    }
+
+    template <typename U>
+    unique_ptr& operator=(unique_ptr<U>&& other) {
+        if ((void*)(this) != (void*)(&other)) {
+            free(ptr);
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        return *this;
+    }
+    
+    ~unique_ptr() {
+        if (ptr)
+            free(ptr);
+    }
+
+    void* operator->() const {
+        return ptr;
+    }
+
+    void* get() const {
+        return ptr;
+    }
+
+    void* release() {
+        void* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+
+    explicit operator bool() const {
+        return ptr != nullptr;
+    }
+
+private:
+    template<typename> friend class unique_ptr;
+    void* ptr;
+};
+
 template <typename T, typename... Args>
 unique_ptr<T> make_unique(Args&&... args) {
     void* raw = malloc(sizeof(T));
