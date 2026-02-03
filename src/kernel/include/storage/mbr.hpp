07189729734cfe6658a256fbf6aaa0a506f8d685
cactoes <maartenhvanrooij@gmail.com>
2026-02-03 11:33:20 +0100

    added mbr functions

--- src/kernel/include/storage/mbr.hpp
+++ src/kernel/include/storage/mbr.hpp
@@ -13,7 +13,7 @@ struct mbr_entry_t {
     uint8_t partition_type;
     char chs_end[3];
     uint32_t lba_start;
-    uint32_t lba_end;
+    uint32_t sector_count;
 } PACKED;
 
 struct mbr_t {
@@ -23,5 +23,8 @@ struct mbr_t {
     mbr_entry_t partitions[MBR_PARTITIONS];
     uint16_t signature;
 } PACKED;
+
+bool mbr_is_entry_valid(const mbr_entry_t* entry);
+bool is_mbr(const uint8_t* data, size_t size);
 
 #endif // __MBR_HPP__
