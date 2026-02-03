e21fcd9b449524a45be283ec978d0ac6fc3794f8
cactoes <maartenhvanrooij@gmail.com>
2026-02-03 11:34:29 +0100

    changes

--- src/kernel/include/storage/vfs.hpp
+++ src/kernel/include/storage/vfs.hpp
@@ -8,6 +8,9 @@
 #include "common.hpp"
 #include "std/string.hpp"
 #include "std/map.hpp"
+#include "std/pointer.hpp"
+#include "storage/drivers/ahci.hpp"
+#include "storage/drivers/ide.hpp"
 
 typedef uint64_t file_descriptor_t;
 
@@ -20,7 +23,7 @@ enum class fs_type_t {
 struct vfs_mount_point_t {
     std::string name;
     fs_type_t type;
-    void* data;
+    std::unique_ptr<void> data;
 };
 
 struct vfs_node_t {
@@ -40,7 +43,7 @@ void set_global_vfs(vfs_t* vfs);
 
 void vfs_init(vfs_t* vfs);
 
-bool vfs_mount_file_system(vfs_t* vfs, const char* name, fs_type_t type, void* fs_data);
+bool vfs_mount_file_system(vfs_t* vfs, const char* name, fs_type_t type, std::unique_ptr<void> fs_data);
 const vfs_mount_point_t* vfs_get_mount_point(vfs_t* vfs, const char* path);
 file_descriptor_t vfs_open_file(vfs_t* vfs, const char* path);
 bool vfs_close_file(vfs_t* vfs, file_descriptor_t fd);
