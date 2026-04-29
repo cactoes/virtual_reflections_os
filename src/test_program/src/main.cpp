96767c60d5a6e702d20d93856d7f2eeb7c38213c
cactoes <maartenhvanrooij@gmail.com>
2026-04-29 20:33:09 +0200

    syscall free

--- src/test_program/src/main.cpp
+++ src/test_program/src/main.cpp
@@ -9,7 +9,9 @@
     RAX = return value
 */
 
-void* syscall_malloc(unsigned long long size) {
+#include "common.hpp"
+
+void* syscall_malloc(size_t size) {
     void* allocated_memory;
     asm volatile (
         "mov $1, %%rax\n\t"
@@ -23,30 +25,26 @@ void* syscall_malloc(unsigned long long size) {
     return allocated_memory;
 }
 
+bool syscall_free(void* ptr) {
+    uint64_t result;
+
+    asm volatile (
+        "mov $2, %%rax\n\t"
+        "mov %1, %%rdi\n\t"
+        "syscall\n\t"
+        "mov %%rax, %0\n\t"
+        : "=r" (result)
+        : "r" (ptr)
+        : "rax", "rdi", "rcx", "r11", "memory"
+    );
+
+    return result == 0;
+}
+
 int main() {
     int* number = (int*)syscall_malloc(sizeof(int));
-
-    *number = 10;
-
-    if (*number == 10) {
-        unsigned long long ret;
-        asm volatile(
-            "mov $10, %%rax\n"
-            "syscall\n"
-            : "=a"(ret)
-            :
-            : "rcx", "r11", "memory"
-        );
-    } else {
-        unsigned long long ret;
-        asm volatile(
-            "mov $100, %%rax\n"
-            "syscall\n"
-            : "=a"(ret)
-            :
-            : "rcx", "r11", "memory"
-        );
-    }
+    syscall_free(number);
+    int* number2 = (int*)syscall_malloc(sizeof(int));
 
     return 0;
 }
