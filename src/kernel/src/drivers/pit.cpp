122e9331a551fe8a13c8496237a3e17cfb1ee797
cactoes <maartenhvanrooij@gmail.com>
2026-05-26 20:40:05 +0200

    fix amd64 dependend code

--- src/kernel/src/drivers/pit.cpp
+++ src/kernel/src/drivers/pit.cpp
@@ -1,7 +1,12 @@
 #include "drivers/pit.hpp"
 #include "std/array.hpp"
-#include "arch/amd64/pit.hpp"
 #include "virtual_thread.hpp"
+
+#include "arch/arch_selector.hpp"
+
+#if CPU_ARCHITECTURE == ARCH_AMD64
+#include "arch/amd64/pit.hpp"
+#endif
 
 static u64 g_tick_count = 0;
 
@@ -18,5 +23,9 @@ u64 pit_get_global_tick_count() {
 }
 
 void pit_init(u16 times_per_second) {
+#if CPU_ARCHITECTURE == ARCH_AMD64
     amd64_pit_init(times_per_second);
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
 }
