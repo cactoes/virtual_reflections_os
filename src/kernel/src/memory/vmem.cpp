b8a3aa7c6601bde65ca85108764803f52b31a4cf
cactoes <maartenhvanrooij@gmail.com>
2026-05-26 20:33:01 +0200

    updated vmem for amd64 selector

--- src/kernel/src/memory/vmem.cpp
+++ src/kernel/src/memory/vmem.cpp
@@ -8,6 +8,7 @@
 #include "interrupt_manager.hpp"
 #include "process.hpp"
 #include "arch/amd64/vmem.hpp"
+#include "arch/arch_selector.hpp"
 
 // 2 * 64 = 128
 // 128 2mb regions
@@ -15,9 +16,11 @@
 static u64 global_mapped_mmio_bitmap[2] {};
 
 void* vmem_virtual_to_physical(void* vaddr) {
-    // TODO @since 20/05/2026 -- 01:05
-    // amd check ...
+#if CPU_ARCHITECTURE == ARCH_AMD64
     return amd64_vmem_virtual_to_physical(vaddr);
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
 }
 
 void* vmem_map_mmio_region(void* paddr) {
@@ -73,47 +76,24 @@ size_t vmem_smart_alloc_pages(const void* vaddr, size_t size, u64 flags) {
     return allocated;
 }
 
-// bool vmem_init(const void* mbstruct) {
-//     const u64 aligned_kernel_end_addr = align_up(LINKER_END_KERNEL_PHYS, PAGE_SIZE_LARGE);
-//     u64 kernel_page_count = aligned_kernel_end_addr / PAGE_SIZE;
-
-//     if (!pmem_try_reserve_address(0, kernel_page_count))
-//         return false;
-
-//     for (auto mm_entry = mb2_get_first_entry((multiboot2_info_t*)mbstruct); mm_entry; mm_entry = mb2_get_next_entry((multiboot2_info_t*)mbstruct, mm_entry)) {
-//         // reserve physical pages for reserved memory
-//         if (mm_entry->type != (u32)memory_map_type_t::USABLE) {
-//             if (!pmem_is_in_memory_range((void*)(mm_entry->addr + mm_entry->len)))
-//                 continue;
-
-//             // reserve as much as possible
-//             for (size_t i = 0; i < mm_entry->len; i += PAGE_SIZE) {
-//                 if (!pmem_try_reserve_address((void*)(align_down(mm_entry->addr, PAGE_SIZE) + i), 1))
-//                     return false;
-//             }
-//         }
-//     }
-
-//     return true;
-// }
-
 bool vmem_unmap_2mb(void* vaddr) {
-    // TODO @since 20/05/2026 -- 01:01
-    // amd64 check bla bla bla
+#if CPU_ARCHITECTURE == ARCH_AMD64
     return amd64_vmem_unmap_2mb(vaddr);
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
 }
 
 bool vmem_map_2mb(const void* vaddr, const void* paddr, u64 flags) {
-    // if amd64 ->
-
+#if CPU_ARCHITECTURE == ARCH_AMD64
     bool is_kernel = BIT_CHECK(flags, VMEM_BIT_USER_KERNEL);
     bool is_readwrite = BIT_CHECK(flags, VMEM_BIT_READWRITE);
     bool is_executable = BIT_CHECK(flags, VMEM_BIT_EXECUTE);
 
     return amd64_vmem_map_2mb(vaddr, paddr, is_kernel, is_readwrite, is_executable);
-
-    // else ->
-    // error
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
 }
 
 bool v2_vmem_map_2mb_remote(struct process_t* process, const void* vaddr, const void* paddr, u64 flags) {
