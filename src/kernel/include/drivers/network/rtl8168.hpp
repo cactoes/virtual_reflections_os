e4b654bcc83cea182e322a7cf3a44420f2f1c50c
cactoes <maartenhvanrooij@gmail.com>
2026-06-03 15:18:48 +0200

    a lot of rtl8168 driver stuff

--- src/kernel/include/drivers/network/rtl8168.hpp
+++ src/kernel/include/drivers/network/rtl8168.hpp
@@ -8,33 +8,80 @@
 #ifndef __DRIVERS_NETWORK_RTL8168_HPP__
 #define __DRIVERS_NETWORK_RTL8168_HPP__
 
-#define RTL8168_MAC         0x0 // size 6
-#define RTL8168_MAR         0x8 // size 8
-#define RTL8168_RBSTART     0x30 // size 4
-#define RTL8168_CMD         0x37 // size 1
-#define RTL8168_CAPR        0x38 // size 2
-#define RTL8168_IMR         0x3C // size 2
-#define RTL8168_ISR         0x3E // size 2
+#define RTL8168_MAC                 0x0
+#define RTL8168_MAR                 0x8
+#define RTL8168_TX_DESC_ADDR_LOW    0x20
+#define RTL8168_TX_DESC_ADDR_HIGH   0x24
+#define RTL8168_RBSTART             0x30
+#define RTL8168_CMD                 0x37
+#define RTL8168_CAPR                0x38
+#define RTL8168_IMR                 0x3C
+#define RTL8168_ISR                 0x3E
+#define RTL8168_TX_CONFIG           0x40
+#define RTL8168_RX_CONFIG           0x44
+#define RTL8168_CFG9346             0x50
+#define RTL8168_RX_MAX_SIZE         0xDA
+#define RTL8168_RX_DESC_ADDR_LOW    0xE4
+#define RTL8168_RX_DESC_ADDR_HIGH   0xE8
 
-#define RTL8168_CONFIG_1    0x52 // 1
+#define RTL8168_CONFIG_1            0x52
 
-#define RTL8168_LWAKE       0x0
-#define RTL8168_LWPTN       0x0
+#define RTL8168_LWAKE               0x0
+#define RTL8168_LWPTN               0x0
+#define RTL8168_CMD_TX_EN           0x04
+#define RTL8168_CMD_RX_EN           0x08
+#define RTL8168_CFG9346_UNLOCK      0xC0
+#define RTL8168_CFG9346_LOCK        0x00
 
-#define RTL8168_CMD_RST     0x10
+#define RTL8168_CMD_RST             0x10
+
+#define RTL8168_TPPOLL 0x38
+
+#define RTL8168_DESC_OWN            (1U << 31)
+#define RTL8168_DESC_EOR            (1U << 30)
+#define RTL8168_DESC_FS             (1U << 29)
+#define RTL8168_DESC_LS             (1U << 28)
+
+#define NUM_TX_DESC                 64
+#define NUM_RX_DESC                 64
+#define RX_BUF_SIZE                 1536
+#define TX_BUF_SIZE                 1536
+
+#define RTL8168_ISR_ROK     0x0001
+#define RTL8168_ISR_RER     0x0002
+#define RTL8168_ISR_TOK     0x0004
+#define RTL8168_ISR_TER     0x0008
+#define RTL8168_ISR_LINKCHG 0x0020
 
 #include "drivers/pcie.hpp"
 #include "memory/heap.hpp"
 #include "common.hpp"
+
+struct rtl8168_desc_t {
+    u32 command;
+    u32 vlan;
+    u32 address_low;
+    u32 address_high;
+} PACKED;
 
 struct rtl8168_t {
     void* mmio_region;
     u8 mac[6];
 
     heap_t* dma_heap;
+
+    rtl8168_desc_t* tdesc_array;
+    void* tx_buffer_array;
+    u64 tx_current;
+
+    rtl8168_desc_t* rdesc_array;
+    void* rx_buffer_array;
+    u64 rx_current;
 };
 
 bool rtl8168_init_device(const pci_device_t* pcie_device, rtl8168_t* network_device);
 bool is_rtl8168_device(const pci_device_t* device);
+bool rtl8168_send_packet(rtl8168_t* device, const void* data, u64 size);
+void rtl8168_generic_handle_interrupt(rtl8168_t* device);
 
 #endif // __DRIVERS_NETWORK_RTL8168_HPP__
--- src/kernel/src/drivers/network/rtl8168.cpp
+++ src/kernel/src/drivers/network/rtl8168.cpp
@@ -3,6 +3,23 @@
 #include "arch/amd64/cpu.hpp"
 #include "io.hpp"
 #include "virtual_thread.hpp"
+#include "interrupt_manager.hpp"
+#include "network/nidm.hpp"
+
+#include "arch/arch_selector.hpp"
+
+#if CPU_ARCHITECTURE == ARCH_AMD64
+
+extern "C" interrupt_t amd64_convert_to_interrupt(u64 code);
+
+void* amd64_rtl8168_handle_interrupt(void* stack, void* data) {
+    rtl8168_generic_handle_interrupt((rtl8168_t*)data);
+    return stack;
+}
+
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
 
 void rtl8168_write_reg32(rtl8168_t* device, u32 offset, u32 value) {
     *((volatile u32*)((u8*)device->mmio_region + offset)) = value;
@@ -10,6 +27,14 @@ void rtl8168_write_reg32(rtl8168_t* device, u32 offset, u32 value) {
 
 u32 rtl8168_read_reg32(rtl8168_t* device, u32 offset) {
     return *((volatile u32*)((u8*)device->mmio_region + offset));
+}
+
+void rtl8168_write_reg16(rtl8168_t* device, u32 offset, u16 value) {
+    *((volatile u16*)((u8*)device->mmio_region + offset)) = value;
+}
+
+u16 rtl8168_read_reg16(rtl8168_t* device, u16 offset) {
+    return *((volatile u16*)((u8*)device->mmio_region + offset));
 }
 
 void rtl8168_write_reg8(rtl8168_t* device, u32 offset, u8 value) {
@@ -37,6 +62,99 @@ bool rtl8168_load_mac(rtl8168_t* device) {
     device->mac[3] = (mac_low >> 24) & 0xff;
     device->mac[4] = (mac_high >> 0) & 0xff;
     device->mac[5] = (mac_high >> 8) & 0xff;
+
+    return true;
+}
+
+bool rtl8168_receive_init(rtl8168_t* network_device) {
+    if (!network_device)
+        return false;
+
+    network_device->rx_current = 0;
+
+    network_device->rdesc_array = (rtl8168_desc_t*)dma_heap_alloc(network_device->dma_heap, sizeof(rtl8168_desc_t) * NUM_RX_DESC, 16);
+    if (!network_device->rdesc_array)
+        return false;
+
+    memzero(network_device->rdesc_array, sizeof(rtl8168_desc_t) * NUM_RX_DESC);
+
+    network_device->rx_buffer_array = (void*)dma_heap_alloc(network_device->dma_heap, RX_BUF_SIZE * NUM_RX_DESC, 16);
+    if (!network_device->rx_buffer_array)
+        return false;
+
+    memzero(network_device->rx_buffer_array, RX_BUF_SIZE * NUM_RX_DESC);
+
+    for (u64 i = 0; i < NUM_RX_DESC; i++) {
+        u8* buffer = ((u8*)network_device->rx_buffer_array) + RX_BUF_SIZE * i;
+
+        u64 physical = (u64)dma_get_physical(network_device->dma_heap, buffer);
+
+        network_device->rdesc_array[i].vlan = 0;
+        network_device->rdesc_array[i].address_low = (u32)physical;
+        network_device->rdesc_array[i].address_high = (u32)(physical >> 32);
+
+        u32 cmd = RX_BUF_SIZE | RTL8168_DESC_OWN;
+        if (i == NUM_RX_DESC - 1)
+            cmd |= RTL8168_DESC_EOR;
+
+        network_device->rdesc_array[i].command = cmd;
+    }
+
+    // upload physical address
+    u64 physical = (u64)dma_get_physical(network_device->dma_heap, network_device->rdesc_array);
+    rtl8168_write_reg32(network_device, RTL8168_RX_DESC_ADDR_LOW, (u32)physical);
+    rtl8168_write_reg32(network_device, RTL8168_RX_DESC_ADDR_HIGH, (u32)(physical >> 32));
+
+    // upload buffer size
+    rtl8168_write_reg16(network_device, RTL8168_RX_MAX_SIZE, RX_BUF_SIZE);
+
+    // upload settings
+    rtl8168_write_reg32(network_device, RTL8168_RX_CONFIG, 0x0f | 0x0700);
+
+    return true;
+}
+
+bool rtl8168_transmit_init(rtl8168_t* network_device) {
+    if (!network_device)
+        return false;
+
+    network_device->tx_current = 0;
+
+    network_device->tdesc_array = (rtl8168_desc_t*)dma_heap_alloc(network_device->dma_heap, sizeof(rtl8168_desc_t) * NUM_TX_DESC, 16);
+    if (!network_device->tdesc_array)
+        return false;
+
+    memzero(network_device->tdesc_array, sizeof(rtl8168_desc_t) * NUM_TX_DESC);
+
+    network_device->tx_buffer_array = (void*)dma_heap_alloc(network_device->dma_heap, TX_BUF_SIZE * NUM_TX_DESC, 16);
+    if (!network_device->tx_buffer_array)
+        return false;
+
+    memzero(network_device->tx_buffer_array, TX_BUF_SIZE * NUM_TX_DESC);
+
+    for (u64 i = 0; i < NUM_TX_DESC; i++) {
+        u8* buffer = ((u8*)network_device->tx_buffer_array) + TX_BUF_SIZE * i;
+
+        u64 physical = (u64)dma_get_physical(network_device->dma_heap, buffer);
+
+        network_device->tdesc_array[i].vlan = 0;
+        network_device->tdesc_array[i].address_low = (u32)physical;
+        network_device->tdesc_array[i].address_high = (u32)(physical >> 32);
+
+        u32 cmd = 0;
+        if (i == NUM_TX_DESC - 1)
+            cmd |= RTL8168_DESC_EOR;
+
+        network_device->tdesc_array[i].command = cmd;
+    }
+
+    // upload physical address
+    u64 physical = (u64)dma_get_physical(network_device->dma_heap, network_device->tdesc_array);
+    rtl8168_write_reg32(network_device, RTL8168_TX_DESC_ADDR_LOW, (u32)physical);
+    rtl8168_write_reg32(network_device, RTL8168_TX_DESC_ADDR_HIGH, (u32)(physical >> 32));
+
+    // upload settings
+    rtl8168_write_reg32(network_device, RTL8168_TX_CONFIG, 0x03000700);
 
     return true;
 }
@@ -73,8 +191,105 @@ bool rtl8168_init_device(const pci_device_t* pcie_device, rtl8168_t* network_dev
     while (rtl8168_read_reg8(network_device, RTL8168_CMD) & RTL8168_CMD_RST)
         vthread_sleep(1);
 
-    // const u32 irq = pci_config_read(pcie_device, PCI_CONFIG_IRQ_LINE) & MAX_UINT8;
+    rtl8168_write_reg8(network_device, RTL8168_CFG9346, RTL8168_CFG9346_UNLOCK);
+
+    if (!rtl8168_receive_init(network_device))
+        return false;
+
+    if (!rtl8168_transmit_init(network_device))
+        return false;
+
+    rtl8168_write_reg16(network_device, RTL8168_IMR, 0x003F);
+
+    rtl8168_write_reg8(network_device, RTL8168_CMD, RTL8168_CMD_TX_EN | RTL8168_CMD_RX_EN);
+
+    rtl8168_write_reg8(network_device, RTL8168_CFG9346, RTL8168_CFG9346_LOCK);
+
+#if CPU_ARCHITECTURE == ARCH_AMD64
+    const u32 irq = pci_config_read(pcie_device, PCI_CONFIG_IRQ_LINE) & MAX_UINT8;
+    if (!hook_interrupt(amd64_convert_to_interrupt(irq + 0x20), amd64_rtl8168_handle_interrupt, (void*)network_device))
+        return false;
+#else
+#error CPU_ARCH_NOT_SUPPORTED
+#endif
     return true;
+}
+
+bool rtl8168_send_packet(rtl8168_t* device, const void* data, u64 size) {
+    if (!device || !data || size > TX_BUF_SIZE)
+        return false;
+
+    u64 index = device->tx_current;
+
+    // last packet still busy
+    if (device->tdesc_array[index].command & RTL8168_DESC_OWN)
+        // we could wait?
+        return false;
+
+    u8* buffer = ((u8*)device->tx_buffer_array) + (TX_BUF_SIZE * index);
+    memcpy(buffer, data, size);
+
+    u32 cmd = (u32)size | RTL8168_DESC_OWN | RTL8168_DESC_FS | RTL8168_DESC_LS; 
+
+    if (index == NUM_TX_DESC - 1)
+        cmd |= RTL8168_DESC_EOR;
+
+    device->tdesc_array[index].command = cmd;
+
+    rtl8168_write_reg8(device, RTL8168_TPPOLL, (1 << 6));
+
+    device->tx_current = (index + 1) % NUM_TX_DESC;
+
+    return true;
+}
+
+DISABLE_SSE void rtl8168_receive_packet(rtl8168_t* device) {
+    while ((device->rdesc_array[device->rx_current].command & RTL8168_DESC_OWN) == 0) {
+        u32 cmd = device->rdesc_array[device->rx_current].command;
+        u32 length = cmd & 0x3FFF;
+        length -= 4;
+        u8* packet = ((u8*)device->rx_buffer_array) + (RX_BUF_SIZE * device->rx_current);
+
+        network_packet_t network_packet {};
+        network_packet.interface = nic_get_interface_from_device(get_global_nic(), device);
+        network_packet.data = (u8*)malloc(length);
+        memcpy(network_packet.data, packet, length);
+        network_packet.size = length;
+        nic_receive_packet(get_global_nic(), network_packet);
+
+        u32 new_cmd = RX_BUF_SIZE | RTL8168_DESC_OWN;
+        if (device->rx_current == NUM_RX_DESC - 1)
+            new_cmd |= RTL8168_DESC_EOR;
+        
+        device->rdesc_array[device->rx_current].command = new_cmd;
+
+        device->rx_current = (device->rx_current + 1) % NUM_RX_DESC;
+    }
+}
+
+void rtl8168_generic_handle_interrupt(rtl8168_t* device) {
+    if (!device)
+        return;
+
+    u16 status = rtl8168_read_reg16(device, RTL8168_ISR);
+
+    if (status == 0)
+        return;
+
+    rtl8168_write_reg16(device, RTL8168_ISR, status);
+
+    if (status & RTL8168_ISR_LINKCHG) {
+        // TODO @since 03/06/2026 -- 15:14
+    }
+
+    if (status & RTL8168_ISR_ROK)
+        rtl8168_receive_packet(device);
+
+    if (status & RTL8168_ISR_TOK) {
+        // TODO @since 03/06/2026 -- 15:15
+        // packet transmit OK
+    }
+
 }
 
 bool is_rtl8168_device(const pci_device_t* device) {
