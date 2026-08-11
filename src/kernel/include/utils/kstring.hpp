dd24f017e3c8b304a0786c882806ca1fae45c5b2
cactoes <maartenhvanrooij@gmail.com>
2026-08-12 00:35:16 +0200

    fixes & cleanup

--- src/kernel/include/utils/kstring.hpp
+++ src/kernel/include/utils/kstring.hpp
@@ -9,22 +9,19 @@
 #define __KSTRING_HPP__
 
 #define KSTRING_MAX_LEN         MAX_UINT64
-#define STATIC_KSTRING(str)     (kstring_t){ (i8*)str, (u32)(sizeof(str) - 1), false }
+#define STATIC_KSTRING(str)     (kstring_t){ (i8*)str, (u64)(sizeof(str) - 1), false }
 
 #include "common.hpp"
 #include "memory/heap.hpp"
-
 
 /// @brief          returns length of string
 /// @param[in] str  string to get length of
 /// @return         length of string excluding nullterminator
 /// @remarks        replacement for the std/string import
-static inline u64 __strlen(const char* cstr) {
-    if (cstr == nullptr || *cstr == '\0')
-        return 0;
-
-    size_t len = 0;
-    while (cstr[len] != '\0')
+static inline
+u64 strlen_internal(const char* cstr) {
+    u64 len = 0;
+    while (cstr && cstr[len] != '\0')
         len++;
 
     return len;
@@ -41,9 +38,9 @@ struct kstring_t {
 
     // for simple compatibility we have these
     kstring_t() : length(0), buffer(nullptr), is_heap(false) {};
-    kstring_t(const char* cstr) : length(__strlen(cstr)), buffer((i8*)cstr), is_heap(false) {};
+    kstring_t(const char* cstr) : length(strlen_internal(cstr)), buffer((i8*)cstr), is_heap(false) {};
     kstring_t(i8* buff, u64 len, bool heap) : length(len), buffer(buff), is_heap(heap) {}
-} __packed;
+};
 
 /// @brief 
 /// @param data 
@@ -87,7 +84,7 @@ kstring_t kstring_copy(const kstring_t* src) {
 
 static
 bool kstring_equals(const kstring_t* kstr1, const kstring_t* kstr2) {
-    if (!kstr1->length != kstr2->length)
+    if (!kstr1 || !kstr2 || kstr1->length != kstr2->length)
         return false;
 
     return memeq(kstr1->buffer, kstr2->buffer, kstr1->length);
@@ -95,10 +92,13 @@ bool kstring_equals(const kstring_t* kstr1, const kstring_t* kstr2) {
 
 static
 kstring_t kstring_substring(const kstring_t* kstr, u64 pos, u64 count = KSTRING_MAX_LEN) {
+    if (!kstr)
+        return {};
+
     if (pos >= kstr->length)
         return {};
 
-    if (pos + count >= kstr->length || count == KSTRING_MAX_LEN)
+    if (count == KSTRING_MAX_LEN || pos + count >= kstr->length)
         count = kstr->length - pos;
 
     return kstring_create(kstr->buffer + pos, count);
@@ -109,14 +109,20 @@ kstring_t kstring_substring(const kstring_t* kstr, u64 pos, u64 count = KSTRING_
 /// @return         pointer to a cstring which HAS to be free'd 
 static
 char* kstring_to_cstring(const kstring_t* kstr) {
+    if (!kstr)
+        return nullptr;
+
     char* cstr = (char*)malloc(kstr->length + 1);
-    memzero(cstr, kstr->length + 1);
     memcpy(cstr, kstr->buffer, kstr->length);
+    cstr[kstr->length] = '\0';
     return cstr;
 }
 
 static
 void kstring_append_simple(kstring_t* kstr, const i8* data, u64 size) {
+    if (!kstr || !data)
+        return;
+
     u64 new_length = kstr->length + size;
 
     i8* newbuffer = (i8*)malloc(new_length);
@@ -136,7 +142,7 @@ void kstring_append_simple(kstring_t* kstr, const i8* data, u64 size) {
 
 static
 void kstring_append(kstring_t* kstr, const char* cstr) {
-    return kstring_append_simple(kstr, (const i8*)cstr, __strlen(cstr));
+    return kstring_append_simple(kstr, (const i8*)cstr, strlen_internal(cstr));
 }
 
 static
@@ -145,12 +151,15 @@ void kstring_append(kstring_t* kstr, const kstring_t* kstr_src) {
 }
 
 static
-std::dynamic_array<kstring_t> kstring_split(const kstring_t& kstr, char ch) {
+std::dynamic_array<kstring_t> kstring_split(const kstring_t* kstr, char ch) {
+    if (!kstr)
+        return {};
+
     std::dynamic_array<kstring_t> parts {};
 
     u64 i = 0;
-    const u64 len = kstr.length;
-    const i8* str = kstr.buffer;
+    const u64 len = kstr->length;
+    const i8* str = kstr->buffer;
 
     while (i < len) {
         while (i < len && str[i] == ch)
@@ -165,7 +174,7 @@ std::dynamic_array<kstring_t> kstring_split(const kstring_t& kstr, char ch) {
             i++;
 
         if (i > start)
-            parts.insert_back(kstring_substring(&kstr, start, i - start));
+            parts.insert_back(kstring_substring(kstr, start, i - start));
     }
 
     return parts;
